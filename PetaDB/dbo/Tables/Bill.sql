ea414a8c0e2c7796b8cad07034fc2130ed56aa9d
Deepak <Deepak@DESKTOP-CLFOV31>
2017-11-18 13:10:45 +0530

    Changes done in user friendly attributes in metadata and created bill table partially

---
+++ PetaDB/dbo/Tables/Bill.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Bill]
+(
+	[BillID] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(350) NULL, 
+    [Date] DATE NULL, 
+    [Address] VARCHAR(350) NULL, 
+    [RoomChargesPerDay] DECIMAL(15, 2) NULL, 
+    [TotalDays] INT NULL, 
+    [NoOfPerson] INT NULL, 
+    [AdvReceiptNo] INT NULL, 
+    [DateArrival] DATE NULL, 
+    [CheckInTime] TIME NULL, 
+    [RegisterNo] INT NULL, 
+    [DateDeparture] DATE NULL, 
+    [RoomNo] INT NULL
+)
